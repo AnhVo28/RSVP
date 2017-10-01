16a563c4c1563538f0c042cf69b50ab97a137e40
HoangAnhVo <vohoanganhkhtn@gmail.com>
2017-10-01 18:09:34 +0300

    Confirmation feature complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GuestList from './GuestList';
+
+
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: 'Tresure',
+        isConfirmed: false
+      },
+      {
+        name: 'Nic',
+        isConfirmed: false
+      },
+      {
+        name: 'Ryan Vo',
+        isConfirmed: true
+      },
+    ]
+  }
+
+  getTotalInvited =()=> this.state.guests.length;
+  // getAttendingGuest =() =>
+  // gitUnconfirmedGuests=() =>
+
+  toggleConfirmationAt= (indexToChange)=>{
+    this.setState({
+      guests: this.state.guests.map((guest, index)=>{
+        if(indexToChange === index){
+          return {
+            ...guest,
+            isConfirmed: !guest.isConfirmed
+          }
+        }
+        return guest;
+      })
+    })
+  }
+
   render() {
     return (
-      <div class="App">
+      <div className="App">
         <header>
           <h1>RSVP</h1>
           <p>A Treehouse App</p>
@@ -14,14 +53,14 @@ class App extends Component {
               <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
-        <div class="main">
+        <div className="main">
           <div>
             <h2>Invitees</h2>
             <label>
               <input type="checkbox"/> Hide those who haven't responded
             </label>
           </div>
-          <table class="counter">
+          <table className="counter">
             <tbody>
               <tr>
                 <td>Attending:</td>
@@ -37,32 +76,7 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li class="pending"><span>Safia</span></li>
-            <li class="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked/> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li class="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked/> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox"/> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList guests={this.state.guests} toggleConfirmationAt={this.toggleConfirmationAt}/>
         </div>
       </div>
     );
